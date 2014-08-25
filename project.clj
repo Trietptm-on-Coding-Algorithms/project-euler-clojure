e6b66d9f6e03a7129400bbeb89e9595e87bd77e4
Do Nhat Minh <mrordinaire@gmail.com>
2014-08-25 22:36:54 +0800

    bumped versions of dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.combinatorics "0.0.6"]
-                 [org.clojure/math.numeric-tower "0.0.2"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.combinatorics "0.0.8"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
